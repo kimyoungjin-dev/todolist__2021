13897d688edc94bb6e4dc7755a38ba7e4cd7a286
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-01-25 20:11:02 +0900

    Passing arguments from todos to todolist in todo.js

--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,34 @@
 import React, { useState } from "react";
+import Todolist from "./TodoList";
 
 const Todo = () => {
-  const [todos, setTodos] = useState(["공부하기"]);
+  const [todos, setTodos] = useState([]);
+  const [newTodos, setNewTodos] = useState("");
 
-  return <div></div>;
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewTodos(value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setTodos([...todos, { id: Date.now(), text: newTodos, checked: false }]);
+    setNewTodos("");
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input type="text" onChange={onChange} value={newTodos} />
+        <input type="submit" value="확인" />
+      </form>
+      <ul>
+        <Todolist todos={todos} />
+      </ul>
+    </>
+  );
 };
 
 export default Todo;
