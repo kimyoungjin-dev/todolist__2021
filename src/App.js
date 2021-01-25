cc1b62fba48fd3669149db91ab05f47df3c7a776
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-01-25 21:47:53 +0900

    Make a mistake and rewrite

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import Clock from "./Clock";
 import Greeting from "./Greeting";
 import Todo from "./Todo";
+
 const App = () => {
   return (
-    <div>
-      <Greeting />
-      <Clock />
-      <Todo />
-    </div>
+    <>
+      <div>
+        <Greeting />
+        <Clock />
+        <Todo />
+      </div>
+    </>
   );
 };
 
