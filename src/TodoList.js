79e640f5fea9460d80926a6fa0e4a8e9a0e3589d
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-01-25 20:11:22 +0900

    Takes todos arguments from todo.js

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,1 +1,14 @@
-rsc;
+import React from "react";
+import TodoItem from "./TodoItem";
+
+const Todolist = ({ todos }) => {
+  return (
+    <div>
+      {todos.map((todo) => (
+        <TodoItem todo={todo} key={todo.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Todolist;
