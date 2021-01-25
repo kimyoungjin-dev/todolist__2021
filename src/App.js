a75da21cfbf24bbaa9d7c6a3a953777b0d3c1ff3
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-01-25 18:07:18 +0900

    Connect Greeting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Clock from "./Clock";
+import Greeting from "./Greeting";
 
 const App = () => {
   return (
     <div>
+      <Greeting />
       <Clock />
     </div>
   );
