82b12af58cf44430cd7d13e206320d3ccfff1add
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-01-26 18:42:41 +0900

    Import bg.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import Clock from "./Clock";
 import Greeting from "./Greeting";
 import Todo from "./Todo";
+import Weather from "./Weather";
+import Bg from "./Bg";
 
 const App = () => {
   return (
     <>
       <div>
+        <Bg />
+        <Weather />
         <Greeting />
         <Clock />
         <Todo />
