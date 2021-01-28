e91b2b288fdf8dcb4d9c0803ce96c8a37d339e3d
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-01-28 17:57:56 +0900

    Write bg.js code

--- src/Bg.js
+++ src/Bg.js
@@ -1,7 +1,18 @@
 import React from "react";
+import image1 from "./images/image1.jpeg";
+import image2 from "./images/image2.jpeg";
+import image3 from "./images/image3.jpeg";
+import image4 from "./images/image4.jpeg";
+import image5 from "./images/image5.jpeg";
 
 const Bg = () => {
-  return <div></div>;
+  const number = Math.floor(Math.random() * 5 + 1);
+  const randomNumber = number === 1 ? image4 : image2;
+  return (
+    <div className="backgroundImage">
+      <img src={randomNumber} />
+    </div>
+  );
 };
 
 export default Bg;
