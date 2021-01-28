28c4685fd987ab762080a2c30fd3e4e019fcf96f
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-01-29 00:58:26 +0900

    Write a function that prints the current date and day of the week Write a Weather Info function
    Write a function that prints the current date and day of the week, and write the WeatherInfo function.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import WeatherIcon from "./WeatherIcon.js";
 
-const WeatherInfo = () => {
-  return <div></div>;
+const WeatherInfo = ({
+  temperature,
+  country,
+  loading,
+  location,
+  condition,
+}) => {
+  const [day, setDay] = useState("");
+
+  const getDay = () => {
+    let array = Array.of(
+      "SUNDAY",
+      "MONDAY",
+      "TUESDAY",
+      "WEDSDAY",
+      "THURSDAY",
+      "FRIDAY",
+      "SATARDAY"
+    );
+    const date = new Date();
+    const day = date.getDay();
+    setDay(array[day]);
+  };
+
+  useEffect(() => {
+    getDay();
+  }, []);
+
+  return (
+    <div className="WeatherInfo">
+      <span className="WeatherInfo__condition">
+        <WeatherIcon condition={condition} />
+      </span>
+      <span>{condition}</span>
+      <span className="WeatherInfo__temperature">{temperature}℃</span>
+      <div>{day}</div>
+    </div>
+  );
 };
 
 export default WeatherInfo;
