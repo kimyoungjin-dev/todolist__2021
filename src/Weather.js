9358e3a382de9f9d92ec999cf0f80a7c057e9135
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-01-29 00:58:53 +0900

    Clear error state, correct loading state

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,29 @@
+import axios from "axios";
+import { array } from "prop-types";
 import React, { useEffect, useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherLoading from "./WeatherLoading";
+const API_KEY = "1cce20274897538f27f72a370b9bb045";
 
 const Weather = () => {
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false); //차후 true 수정
+  const [temperature, setTemperature] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [condition, setCondition] = useState(null);
+  const [country, setCountry] = useState(null);
 
-  const getWeather = (lat, long) => {};
+  const getWeather = async (lat, long) => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`
+    );
+
+    setTemperature(data.main.temp);
+    setLocation(data.name);
+    setCondition(data.weather[0].main);
+    setCountry(data.sys.country);
+    setLoading(false);
+  };
+
   const getLocation = () => {
     try {
       navigator.geolocation.getCurrentPosition((position) => {
@@ -14,7 +32,7 @@ const Weather = () => {
         getWeather(lat, long);
       });
     } catch (error) {
-      setError(error);
+      array("현재위치를 찾을수없습니다.", "다시시도해주세요");
     }
   };
 
@@ -23,8 +41,17 @@ const Weather = () => {
   }, []);
   return (
     <>
-      {loading ? "Loading... " : <WeatherInfo />}
-      <div>{error}</div>
+      {loading ? (
+        <WeatherLoading />
+      ) : (
+        <WeatherInfo
+          loading={loading}
+          temperature={temperature}
+          country={country}
+          location={location}
+          condition={condition}
+        />
+      )}
     </>
   );
 };
