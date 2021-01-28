80ace0dd8dce14181678d384a89cf1b867569bb4
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-01-28 17:58:20 +0900

    Create weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import WeatherInfo from "./WeatherInfo";
 
 const Weather = () => {
-  return <div></div>;
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const getWeather = (lat, long) => {};
+  const getLocation = () => {
+    try {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+        getWeather(lat, long);
+      });
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+  return (
+    <>
+      {loading ? "Loading... " : <WeatherInfo />}
+      <div>{error}</div>
+    </>
+  );
 };
 
 export default Weather;
